728f36b9dce13ee8b7a47108439a4e6fe1a2ecc7
dcuartielles <dcuartielles@gmail.com>
2018-01-04 13:08:37 +0000

    Update install_test_suite.sh

--- install_test_suite.sh
+++ install_test_suite.sh
@@ -8,6 +8,10 @@ testSuiteFolder="decode-suite/"
 
 ptsFolder="/var/lib/phoronix-test-suite/test-suites/local/"
 
+if [ ! -d ${ptsFolder}${testSuiteFolder} ] ; then
+        mkdir ${ptsFolder}${testSuiteFolder}
+fi
+
 cp ${testToMove} ${ptsFolder}${testSuiteFolder}"suite-definition.xml"
 
 # patch the configuration file
@@ -16,4 +20,4 @@ cp /etc/phoronix-test-suite.xml ./phoronix-test-suite.xml.old
 
 cp phoronix-test-suite.xml /etc/phoronix-test-suite.xml
 
-sed -i 's/BOARD_NAME/${boardName}/g' /etc/phoronix-test-suite.xml
+sed -i "s/BOARD_NAME/${boardName}/g" /etc/phoronix-test-suite.xml
