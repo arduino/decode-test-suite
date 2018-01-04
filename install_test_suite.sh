0ba3d9512ae766116897e589172217851422862d
dcuartielles <dcuartielles@gmail.com>
2018-01-04 11:02:20 +0000

    added the creation of folder

--- install_test_suite.sh
+++ install_test_suite.sh
@@ -6,4 +6,6 @@ testSuiteFolder="decode-suite/"
 
 ptsFolder="/var/lib/phoronix-test-suite/test-suites/local/"
 
+mkdir ${ptsFolder}${testSuiteFolder}
+
 cp ${testToMove} ${ptsFolder}${testSuiteFolder}"suite-definition.xml"
